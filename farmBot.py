8c0f104568b81da0092ae9ea80356e79a5938273
Hodeem <miller.hodeem@gmail.com>
2021-09-03 01:34:54 -0500

    Connected script to Heroku Postgres and setup timer to run every Monday at 9 am

--- farmBot.py
+++ farmBot.py
@@ -1,51 +1,56 @@
 import tweepy
-import mysql.connector
-from mysql.connector import Error
+import schedule
+import os
+import psycopg2
 
-try:
-    connection = mysql.connector.connect(host='localhost',
-                                         database='farm',
-                                         user='root',
-                                         password='')
-    if connection.is_connected():
+from dotenv import load_dotenv
 
-        cursor = connection.cursor()
-        cursor.execute("SELECT MAX(date) FROM `datestable`;")
+load_dotenv()
+
+def printData():
+    try:
+        DATABASE_URL = os.getenv('DATABASE_URL')
+
+        con = psycopg2.connect(DATABASE_URL, sslmode='require')
+        cursor = con.cursor()
+
+        cursor.execute("SELECT MAX(dates) FROM farmdates;")
         lastDate = cursor.fetchone()[0]
         formattedDate = lastDate.strftime("%b %d, %Y")
         print("Date ", lastDate)
-    
-        cursor.execute(f'SELECT high, commodity, parish, variety FROM `farmtable` WHERE high=(SELECT MAX(high) from `farmtable` WHERE date="{lastDate}") AND date="{lastDate}";')
+
+        cursor.execute(f"SELECT high, commodity, parish, variety FROM farmdata WHERE high=(SELECT MAX(high) from farmdata WHERE date='{lastDate}') AND date='{lastDate}';")
         max = cursor.fetchone()
 
-        cursor.execute(f'SELECT low, commodity, parish, variety FROM `farmtable`  WHERE low=(SELECT MIN(low) from `farmtable` WHERE date="{lastDate}") AND date="{lastDate}";')
+        cursor.execute(f"SELECT low, commodity, parish, variety FROM farmdata WHERE low=(SELECT MIN(low) from farmdata WHERE date='{lastDate}') AND date='{lastDate}';")
         min = cursor.fetchone()
 
         maxSentence = f"The {max[3]} variety of {max[1]} in {max[2]} is the most expensive commodity published in JAMIS' 'Week Ending - {formattedDate}' report, at ${max[0]} per kg."
         minSentence = f"The {min[3]} variety of {min[1]} in {min[2]} is the least expensive commodity published in JAMIS' 'Week Ending - {formattedDate}' report, at ${min[0]} per kg."
 
-except Error as e:
-    print("Error while connecting to MySQL", e)
-finally:
-    if connection.is_connected():
-        cursor.close()
-        connection.close()
-        print("MySQL connection is closed")
+        print("Max ", maxSentence)
+        print("Min ", minSentence)
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+
+schedule.every().monday.at("9:00").do(printData)
+
+while True:
+    schedule.run_pending()
 
 
+# # Authenticate to Twitter
+# auth = tweepy.OAuthHandler(os.getenv("API_KEY"), os.getenv("API_SECRET"))
+# auth.set_access_token(os.getenv("ACCESS_TOKEN"), os.getenv("ACCESS_TOKEN_SECRET"))
 
-# Authenticate to Twitter
-auth = tweepy.OAuthHandler(os.environ("API_KEY"), os.environ("API_SECRET"))
-auth.set_access_token(os.environ("ACCESS_TOKEN"),    os.environ("ACCESS_TOKEN_SECRET"))
+# # Create API object
+# api = tweepy.API(auth, wait_on_rate_limit=True,
+#     wait_on_rate_limit_notify=True)
 
-# Create API object
-api = tweepy.API(auth, wait_on_rate_limit=True,
-    wait_on_rate_limit_notify=True)
-
-try:
-    api.verify_credentials()
-    print("Authentication OK")
-    api.update_status(maxSentence)
-    api.update_status(minSentence)
-except Exception as e:
-    print("An error occured: " + str(e))
+# try:
+#     api.verify_credentials()
+#     print("Authentication OK")
+#     api.update_status(maxSentence)
+#     api.update_status(minSentence)
+# except Exception as e:
+#     print("An error occured: " + str(e))
