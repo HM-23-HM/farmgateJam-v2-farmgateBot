7b8c2c86563c50c45ac9df6947f5d0fc337def10
Hodeem <miller.hodeem@gmail.com>
2021-09-06 10:03:38 -0500

    Re-arranged code

--- farmBot.py
+++ farmBot.py
@@ -7,6 +7,27 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
+def postStatus(maxSentence, minSentence):
+
+    # Authenticate to Twitter
+    auth = tweepy.OAuthHandler(os.getenv("API_KEY"), os.getenv("API_SECRET"))
+    auth.set_access_token(os.getenv("ACCESS_TOKEN"),
+                      os.getenv("ACCESS_TOKEN_SECRET"))
+
+    # Create API object
+    api = tweepy.API(auth, wait_on_rate_limit=True,
+                    wait_on_rate_limit_notify=True)
+
+    try:
+        api.verify_credentials()
+        print("Authentication OK")
+        api.update_status(maxSentence)
+        api.update_status(minSentence)
+    except Exception as e:
+        print("An error occured: " + str(e))
+
+
 def printData():
     try:
         DATABASE_URL = os.getenv('DATABASE_URL')
@@ -17,42 +38,29 @@ def printData():
         cursor.execute("SELECT MAX(dates) FROM farmdates;")
         lastDate = cursor.fetchone()[0]
         formattedDate = lastDate.strftime("%b %d, %Y")
-        print("Date ", lastDate)
+        # print("Date ", lastDate)
 
-        cursor.execute(f"SELECT high, commodity, parish, variety FROM farmdata WHERE high=(SELECT MAX(high) from farmdata WHERE date='{lastDate}') AND date='{lastDate}';")
+        cursor.execute(
+           f"SELECT high, commodity, parish, variety FROM farmdata WHERE high=(SELECT MAX(high) from farmdata WHERE date='{lastDate}') AND date='{lastDate}';")
         max = cursor.fetchone()
 
-        cursor.execute(f"SELECT low, commodity, parish, variety FROM farmdata WHERE low=(SELECT MIN(low) from farmdata WHERE date='{lastDate}') AND date='{lastDate}';")
+        cursor.execute(
+           f"SELECT low, commodity, parish, variety FROM farmdata WHERE low=(SELECT MIN(low) from farmdata WHERE date='{lastDate}') AND date='{lastDate}';")
         min = cursor.fetchone()
 
         maxSentence = f"The {max[3]} variety of {max[1]} in {max[2]} is the most expensive commodity published in JAMIS' 'Week Ending - {formattedDate}' report, at ${max[0]} per kg."
         minSentence = f"The {min[3]} variety of {min[1]} in {min[2]} is the least expensive commodity published in JAMIS' 'Week Ending - {formattedDate}' report, at ${min[0]} per kg."
 
-        print("Max ", maxSentence)
-        print("Min ", minSentence)
+        postStatus(maxSentence=maxSentence, minSentence=minSentence)
+        # print("Max ", maxSentence)
+        # print("Min ", minSentence)
 
         cursor.close()
     except:
         print("Unexpected error:", sys.exc_info()[0])
 
-schedule.every().monday.at("09:50").do(printData)
+
+schedule.every().monday.at("10:08").do(printData)
 
 while True:
     schedule.run_pending()
-
-
-# Authenticate to Twitter
-auth = tweepy.OAuthHandler(os.getenv("API_KEY"), os.getenv("API_SECRET"))
-auth.set_access_token(os.getenv("ACCESS_TOKEN"), os.getenv("ACCESS_TOKEN_SECRET"))
-
-# Create API object
-api = tweepy.API(auth, wait_on_rate_limit=True,
-    wait_on_rate_limit_notify=True)
-
-try:
-    api.verify_credentials()
-    print("Authentication OK")
-    api.update_status(maxSentence)
-    api.update_status(minSentence)
-except Exception as e:
-    print("An error occured: " + str(e))
